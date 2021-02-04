93b2b5c18bb0c4d461a80648e8bd3e47762b8333
Matthias Nagel <matthinagel@gmail.com>
2021-02-04 11:33:58 +0100

    Runs minimal test

--- gcp/Storage.py
+++ gcp/Storage.py
@@ -1,12 +1,11 @@
 from google.cloud.storage import Client, Blob
 from io import StringIO
 
-
 class Bucket:
     def __init__(self, name):
         client = Client()
         self.bucket = client.get_bucket(name)
     def saveStringToBlob(self, blobname, string):
-        blob = Blob(blobname, bucket)
+        blob = Blob(blobname, self.bucket)
         content = StringIO(string)
         blob.upload_from_file(content)
--- tradegatePipeline/extractTradegate/test/test_scrapeTradegate.py
+++ tradegatePipeline/extractTradegate/test/test_scrapeTradegate.py
@@ -3,13 +3,22 @@ from unittest.mock import Mock, MagicMock, patch
 
 import sys
 sys.modules['google.cloud.storage'] = MagicMock()
+sys.modules['requests'] = MagicMock()
 
 from scrapeTradegate import pubsubEntryPoint
+
+class Stock:
+    def __init__(self, isin):
+        self.isin = isin
 
 @patch('builtins.print', Mock())
 class testScrape(TestCase):
     def testMockRun(self):
-        sqlSourcePatch = patch('scrapeTradegate.StdSource', MagicMock())
-        with sqlSourcePatch:
+        source = Mock()
+        source.query.return_value = [Stock('US3434'), Stock('DE78322')]
+        sqlSourcePatch = patch('scrapeTradegate.StdSource', return_value=source)
+        #source.query().limit().return_value = ['US3434', 'DE78322']
+        strIO = patch('gcp.Storage.StringIO', Mock)
+        with sqlSourcePatch, strIO:
             pubsubEntryPoint(None, None)
 
