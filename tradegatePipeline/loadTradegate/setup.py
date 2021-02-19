9757ec93b07c68625ea9e286c228da19d5eafa0c
Matthias Nagel <matthinagel@gmail.com>
2021-02-19 10:39:43 +0100

    Adds pymysql dependency to setup.py

--- tradegatePipeline/loadTradegate/setup.py
+++ tradegatePipeline/loadTradegate/setup.py
@@ -4,7 +4,8 @@ setuptools.setup(
     name='loadTradegatePipeline',
     version='0.1',
     install_requires=[
-        'sqlalchemy'
+        'sqlalchemy',
+        'pymysql'
     ],
     packages={'gcp':'gcp'}
     #setuptools.find_namespace_packages()
