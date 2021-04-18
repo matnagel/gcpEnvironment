25a95bdb88237a5fdcd3fd37c8c4947bd74eccf9
Matthias Nagel <matthinagel@gmail.com>
2021-04-18 14:55:28 +0200

    Adds time check for valheim server start

--- appEngine/main.py
+++ appEngine/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
+
 import os
+import datetime
+
 from google.cloud import pubsub_v1
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -19,20 +22,33 @@ def cert():
     gets caught by app.yaml. This gets never called"""
     return 'cert - Hello World!'
 
+def checkValheimPreconditions(secret_input):
+    secret = os.getenv('VALHEIMSECRET')
+    if not secret == secret_input:
+        return 'Wrong secret'
+
+    now_time = datetime.now().time()
+    time_cond = now_time >= time(17, 0) and now_time <= time(21, 30)
+    if not time_cond:
+        return f'{now_time} not between 17:00 and 21:30'
+    return False
+
+
+
 @app.route('/valheim/start')
 def valheim():
     """Starts the valheim server"""
-    secret = os.getenv('VALHEIMSECRET')
     secret_input = request.args.get('secret')
-    if secret == secret_input:
+    error = checkValheimPreconditions(secret_input)
+    if error:
+        return error 
+    else:    
         topic_name = os.getenv('VALHEIMTOPIC')
         topic_path = publisher.topic_path(
                 os.environ['GOOGLE_CLOUD_PROJECT'],
                 topic_name)
         publisher.publish(topic_path, data=b'start')
         return f"Message to {topic_name} to start server was sent"
-    else:
-        return 'Wrong secret'
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
